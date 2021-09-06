ec4bb5404570f81aba1b855afc516cf2f04a040e
arnottrcaiado <arnottrcaiado@fac.pe.senac.br>
2021-09-06 20:03:52 +0000

    modificacao trocando button por input no html interacao. mudancas .py

--- mysite/picdigai.py
+++ mysite/picdigai.py
@@ -43,6 +43,26 @@ def escolha_palavra():
 
         return json.dumps({'Digai':'ok', 'Palavras':palavras }, ensure_ascii=False)
 
+@app.route('/digaidesafio',  methods=['GET', 'POST'])
+def escolha_desafio():
+    if request.method == 'GET':
+        cor ="vermelho"
+        if request.args.get('cor') != None :
+            cor = request.args.get('cor')
+        return render_template('digai_desafio_input.html', cor=cor )
+    if request.method == 'POST':
+        palavras=[]
+        for i in range (0,7 ):
+            vlinha = "d"+str(i+1)
+            for j in range (0,1) :
+                variavel = vlinha + str(j+1)
+                if request.form.get( variavel ) != None :
+                    if len(request.form.get( variavel )) > 1 :
+                        palavras.append( request.form.get( variavel ))
+
+        return json.dumps({'DigaiDesafio':'ok', 'Desafios':palavras }, ensure_ascii=False)
+
+
   #      if request.form.get('p11') != None :
   #         palavras.append(request.form.get('p11'))
   #      if request.form.get('p12') != None :
@@ -62,7 +82,38 @@ def escolha_palavra():
 
 @app.route('/digai/interacao', methods=['GET', 'POST'])
 def interacao():
-    return render_template('digai_interacao.html')
+    nome="* Estudante *"
+    secao="# Ab01"
+    ninter = 100
+    cor ="vermelho"
+    if request.method == 'GET':
+        if request.args.get('cor') != None :
+            cor = request.args.get('cor')
+
+        return render_template('digai_interacao.html', cor=cor, nome=nome, secao=secao, ninter=ninter)
+    if request.method == 'POST' :
+        who=[]
+        dig=[]
+        if request.form.get('all') != None :
+            who.append('all')
+        if request.form.get('you') != None :
+            who.append('you')
+        if request.form.get('teacher') != None :
+            who.append('teacher')
+        if request.form.get('power') != None :
+            dig.append('power')
+        if request.form.get('answer') != None :
+            dig.append('answer')
+        if request.form.get('question') != None :
+            dig.append('question')
+        if request.form.get('gifts') != None :
+            dig.append('gifts')
+        if request.form.get('challenge') != None :
+            dig.append('challenge')
+        if request.form.get('help') != None :
+            dig.append('help')
+
+        return json.dumps({'Estudante': nome, 'Secao': secao, 'Inter:': ninter,'DigaiInteracao':'ok', 'Who':who, 'Dig': dig }, ensure_ascii=False)
 
 @app.route('/digai')
 def segunda():
